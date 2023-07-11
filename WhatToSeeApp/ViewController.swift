079ad26f9c312101b3e4a3a842d032d7e41f134e
Artem Sergeev <sergeev190391@gmail.com>
2023-07-11 12:22:05 +0300

    Initial Commit

---
+++ WhatToSeeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToSeeApp
+//
+//  Created by Артем Сергеев on 11.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
