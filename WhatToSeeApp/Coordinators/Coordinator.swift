de92bf81859c7feab3907fcb62e1eb370b1c4535
Artem Sergeev <sergeev190391@gmail.com>
2023-08-17 12:19:20 +0300

    coordinator

---
+++ WhatToSeeApp/Coordinators/Coordinator.swift
@@ -0,0 +1,25 @@
+//
+//  Coordinator.swift
+//  WhatToSeeApp
+//
+//  Created by Артем Сергеев on 16.08.2023.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+  
+  var navigationController: UINavigationController { get }
+  
+  func start()
+}
+
+
+extension Coordinator {
+
+  var navigationController: UINavigationController {
+    return  UINavigationController()
+  }
+
+}
+
